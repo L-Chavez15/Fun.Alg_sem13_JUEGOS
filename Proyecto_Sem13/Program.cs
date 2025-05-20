5a92cac15261f5a86932ba7ec20f62fae22bbe28
Leonardo <leochavezg15@gmail.com>
2025-05-19 21:56:08 -0500

    Creamos el metodo del primer juego

--- Proyecto_Sem13/Program.cs
+++ Proyecto_Sem13/Program.cs
@@ -31,6 +31,63 @@ namespace Proyecto_Sem13
             Console.Write("");
             int inicio = int.Parse(Console.ReadLine());
             Console.Clear();
+
+            switch (inicio)
+            {
+                case 1:
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(" ------------------------------------------------------------------------");
+                    Console.WriteLine("|******************************* JUEGOS *********************************|");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine("|                        SELECCIONE UN JUEGO                             |");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine("|                      > 1)ADIVINA LA PALABRA                            |");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine("|                      > 2)ADIVINA EL NÚMERO                             |");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine("|                      > 3)TRES EN RAYA                                  |");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine("|                      > 0)SALIR                                         |");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine(" ------------------------------------------------------------------------\n");
+                    Console.WriteLine("");
+                    int juego = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    switch (juego)
+                    {
+                        case 1:
+                            
+                            break;
+                        case 2:
+                            
+                            break;
+                        case 3:
+                            
+                            break;
+                        default:
+                            
+                            break;
+                    }
+                    break;
+                case 2:
+                    
+                    break;
+                default:
+                    
+                    break;
+            }
+        }
+        static void JuegoAdivinalaPalabra()
+        {
+            Console.WriteLine("Adivina la palabra de 5 letras");
+            Console.WriteLine("Tienes 6 intentos\n");
+            Console.WriteLine("Pistas:");
+            Console.WriteLine("✓ = Letra correcta en posición correcta");
+            Console.WriteLine("~ = Letra correcta en posición incorrecta");
+            Console.WriteLine("× = Letra no existe en la palabra\n");
+            Console.ReadKey();
+
         }
     }
 }

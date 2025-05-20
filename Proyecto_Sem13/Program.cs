c56525abe329dcd168df6dee75424a017ca09e54
Leonardo <leochavezg15@gmail.com>
2025-05-19 21:53:56 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto_Sem13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Sem13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" ------------------------------------------------------------------------");
+            Console.WriteLine("|************************************************************************|");
+            Console.WriteLine("|*                                                                      *|");
+            Console.WriteLine("|*                                                                      *|");
+            Console.WriteLine("|*                                                                      *|");
+            Console.WriteLine("|*                             > 1)INICIAR                              *|");
+            Console.WriteLine("|*                                                                      *|");
+            Console.WriteLine("|*                             > 2)SALIR                                *|");
+            Console.WriteLine("|*                                                                      *|");
+            Console.WriteLine("|*                                                                      *|");
+            Console.WriteLine("|*                                                                      *|");
+            Console.WriteLine("|*                                                                      *|");
+            Console.WriteLine("|************************************************************************|");
+            Console.WriteLine(" ------------------------------------------------------------------------\n");
+            Console.Write("");
+            int inicio = int.Parse(Console.ReadLine());
+        }
+    }
+}

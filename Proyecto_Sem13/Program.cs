4b7d977a88e01b23beb649a689e5c7fb8c844e9d
Leonardo <leochavezg15@gmail.com>
2025-05-19 22:08:27 -0500

    generamos el partado de selección de juegos

--- Proyecto_Sem13/Program.cs
+++ Proyecto_Sem13/Program.cs
@@ -32,6 +32,36 @@ namespace Proyecto_Sem13
             Console.Write("");
             int inicio = int.Parse(Console.ReadLine());
             Console.Clear();
+
+            switch (inicio)
+            {
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(" ------------------------------------------------------------------------");
+                    Console.WriteLine("|******************************* JUEGOS *********************************|");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine("|                        SELECCIONE UN JUEGO                             |");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine("|                      > 1)ADIVINA LA PALABRA                            |");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine("|                      > 2)ADIVINA EL NÚMERO                             |");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine("|                      > 3)TRES EN RAYA                                  |");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine("|                      > 0)SALIR                                         |");
+                    Console.WriteLine("|                                                                        |");
+                    Console.WriteLine(" ------------------------------------------------------------------------\n");
+                    Console.WriteLine("");
+                    int juego = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    break;
+                case 2:
+                    break;
+                
+                default:
+                    break;
+            }
+
         }
            
     }

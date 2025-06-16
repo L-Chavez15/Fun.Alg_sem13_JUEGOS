edffd1b5812b4cc1c31163faa2e2aa7197f6458c
Leonardo Chávez <n00449821pn.pe>
2025-06-15 22:20:18 -0500

    mejoramos salida

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -52,6 +52,8 @@ namespace ClassLibrary1
         public void salida()
         {
             Console.WriteLine("\t\t\t\t*********** ESPERAMOS VERTE DE NUEVO ***********\t\t\t\t \n");
+            Console.WriteLine("\t\t\t\t\t*********** ADIOS ***********\t\t\t\t\t\t \n");
+            Console.ReadKey();
         }
     }
 }
